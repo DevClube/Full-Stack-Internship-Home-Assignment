dc6e1d17d2390174f965c8fb050e1890a1aa119d
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2024-01-08 22:21:22 +0100

    update entity

--- backend/src/main/java/ma/dnaengineering/backend/models/employes.java
+++ backend/src/main/java/ma/dnaengineering/backend/models/employes.java
@@ -1,8 +1,6 @@
 package ma.dnaengineering.backend.models;
 
 import jakarta.persistence.*;
-
-import jakarta.persistence.Entity;
 @Entity
 public class employes {
     @Id
@@ -10,7 +8,40 @@ public class employes {
     private String employeeName;
     private String jobTitle;
     private double salary;
+
     public employes() {
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
     
     
