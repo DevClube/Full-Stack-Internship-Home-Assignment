a8d8be30174dc41ea2a9ebe5dd3a04bd945cad7d
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2024-01-10 20:30:59 +0100

    backend whole changes

--- backend/src/main/java/ma/dnaengineering/backend/repositories/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.dnaengineering.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ma.dnaengineering.backend.models.employes;
-
-public interface EmployeeRepository extends JpaRepository<employes, Long> {
-    // You can add custom query methods here if needed
-}
